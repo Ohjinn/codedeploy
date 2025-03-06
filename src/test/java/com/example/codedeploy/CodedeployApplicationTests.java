2b91943e2d7554f4c522d0861a2e65e3e64bcc3e
Ohjinn <hjin9445@gmail.com>
2025-03-06 14:43:59 +0900

    first_commit

---
+++ src/test/java/com/example/codedeploy/CodedeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
